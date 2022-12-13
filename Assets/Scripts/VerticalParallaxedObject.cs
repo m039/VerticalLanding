bb30d0e02d34836d382df58d3397fa3d7ed2e724
Dmitry Mozgin <m0391n@gmail.com>
2022-12-13 22:41:37 +0300

    Added VerticalParallaxedObject.

---
+++ Assets/Scripts/VerticalParallaxedObject.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SF
+{
+    public class VerticalParallaxedObject : MonoBehaviour
+    {
+        #region Inspector
+
+        public float speed = 0.1f;
+
+        #endregion
+
+        Vector3 _initPosition;
+
+        Vector3 _cameraInitPosition;
+
+        void Start()
+        {
+            _initPosition = transform.position;
+            _cameraInitPosition = Camera.main.transform.position;
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            var p = Camera.main.transform.position - _cameraInitPosition;
+            p.z = 0;
+
+            transform.position = _initPosition + speed * p;
+        }
+    }
+}
