d0f2e9bd6fd3f6a455df4c6b2aa8e2f67b4ff5ed
Dmitry Mozgin <m0391n@gmail.com>
2022-12-18 18:19:32 +0300

    Added OffsetOnMouseButtonPressed.

---
+++ Assets/Scripts/Menu/MainMenuManager.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SF
+{
+    public class MainMenuManager : MonoBehaviour
+    {
+        #region Inspector
+
+        [SerializeField] RectTransform _MainScreen;
+
+        [SerializeField] RectTransform _LevelSelectionScreen;
+
+        #endregion
+
+        public void OnPlayClicked()
+        {
+
+        }
+
+        public void OnHelpClicked()
+        {
+
+        }
+    }
+}
---
+++ Assets/Scripts/UI/OffsetOnMouseButtonPressed.cs
@@ -0,0 +1,78 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace SF
+{
+    public class OffsetOnMouseButtonPressed : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
+    {
+        #region Inspector
+
+        [SerializeField] RectTransform[] _Transforms;
+
+        [SerializeField] float _Offset;
+
+        #endregion
+
+        float[] _initOffsets;
+
+        void Awake()
+        {
+            if (_Transforms == null || _Transforms.Length <= 0)
+                return;
+
+            _initOffsets = new float[_Transforms.Length];
+            for (int i = 0; i < _Transforms.Length; i++)
+            {
+                _initOffsets[i] = _Transforms[i].anchoredPosition.y;
+            }
+        }
+
+        public void OnPointerDown(PointerEventData eventData)
+        {
+            if (eventData.button == PointerEventData.InputButton.Left)
+            {
+                StartOffset();
+            }
+        }
+
+        public void OnPointerUp(PointerEventData eventData)
+        {
+            if (eventData.button == PointerEventData.InputButton.Left) { 
+                EndOffset();
+            }
+        }
+
+        void StartOffset()
+        {
+            if (_Transforms == null || _Transforms.Length <= 0)
+                return;
+
+            for (int i = 0; i < _Transforms.Length; i++)
+            {
+                var tr = _Transforms[i];
+
+                var p = tr.anchoredPosition;
+                p.y = _initOffsets[i] + _Offset;
+                tr.anchoredPosition = p;
+            }
+        }
+
+        void EndOffset()
+        {
+            if (_Transforms == null || _Transforms.Length <= 0)
+                return;
+
+            for (int i= 0; i < _Transforms.Length; i++)
+            {
+                var tr = _Transforms[i];
+
+                var p = tr.anchoredPosition;
+                p.y = _initOffsets[i];
+                tr.anchoredPosition = p;
+            }
+        }
+    }
+}
