6741c80330739c9259d0e978c09dfe6e433edc56
Dmitry Mozgin <m0391n@gmail.com>
2022-12-18 19:20:02 +0300

    Added appear or disappear animation to MainMenuManager.

--- Assets/Scripts/Menu/MainMenuManager.cs
+++ Assets/Scripts/Menu/MainMenuManager.cs
@@ -1,3 +1,4 @@
+using m039.Common;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,22 +7,126 @@ namespace SF
 {
     public class MainMenuManager : MonoBehaviour
     {
+        enum AppearSide
+        {
+            ToRight, ToLeft
+        }
+
         #region Inspector
 
         [SerializeField] RectTransform _MainScreen;
 
         [SerializeField] RectTransform _LevelSelectionScreen;
 
+        [SerializeField] float _AnimationDuration = 1f;
+
         #endregion
+
+        RectTransform _currentScreen;
+
+        RectTransform[] _screens;
+
+        void Awake()
+        {
+            _screens = new []
+            {
+                _MainScreen,
+                _LevelSelectionScreen
+            };
+
+            ShowScreen(_MainScreen, true);
+        }
 
         public void OnPlayClicked()
         {
-
+            ShowScreen(_LevelSelectionScreen, false, AppearSide.ToLeft);
         }
 
         public void OnHelpClicked()
         {
 
+        }
+
+        public void OnMenuClicked()
+        {
+            ShowScreen(_MainScreen, false, AppearSide.ToRight);
+        }
+
+        void ShowScreen(RectTransform screen, bool force, AppearSide sides = AppearSide.ToLeft)
+        {
+            if (force)
+            {
+                _screens.ForEach(s =>
+                {
+                    s.gameObject.SetActive(screen == s);
+                    s.GetComponent<CanvasGroup>().alpha = 1f;
+                });
+                _currentScreen = screen;
+            } else
+            {
+                IEnumerator animate(RectTransform screen, bool appearOrDisappear)
+                {
+                    screen.gameObject.SetActive(true);
+                    var canvasGroup = screen.GetComponent<CanvasGroup>();
+                    float startAlpha;
+                    float endAlpha;
+                    float startXPosition;
+                    float endXPosition;
+                    var size = screen.sizeDelta;
+                    var d = 0f;
+
+                    if (appearOrDisappear)
+                    {
+                        startAlpha = 0f;
+                        endAlpha = 1f;
+
+                        if (sides == AppearSide.ToRight)
+                        {
+                            startXPosition = -size.x;
+                            endXPosition = 0f;
+                        } else
+                        {
+                            startXPosition = size.x;
+                            endXPosition = 0f;
+                        }
+
+                    } else
+                    {
+                        startAlpha = 1f;
+                        endAlpha = 0f;
+
+                        if (sides == AppearSide.ToRight)
+                        {
+                            startXPosition = 0f;
+                            endXPosition = size.x;
+                        }
+                        else
+                        {
+                            startXPosition = 0f;
+                            endXPosition = -size.x;
+                        }
+                    }
+
+                    while (d < _AnimationDuration)
+                    {
+                        var t = d / _AnimationDuration;
+
+                        canvasGroup.alpha = EasingFunction.EaseInOutCubic(startAlpha, endAlpha, t);
+
+                        var p = screen.anchoredPosition;
+                        p.x = EasingFunction.EaseInOutCubic(startXPosition, endXPosition, t);
+                        screen.anchoredPosition = p;
+
+                        d += Time.deltaTime;
+                        yield return null;
+                    }
+
+                    screen.gameObject.SetActive(appearOrDisappear);
+                }
+
+                StopAllCoroutines();
+                _screens.ForEach(s => StartCoroutine(animate(s, s == screen)));
+            }
         }
     }
 }
