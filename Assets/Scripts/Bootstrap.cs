b681918a5d12177e6fffc8797ae6a2161ec91fc6
Dmitry Mozgin <m0391n@gmail.com>
2024-07-20 16:20:35 +0300

    Make one function prettier

--- Assets/Scripts/Bootstrap.cs
+++ Assets/Scripts/Bootstrap.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -5,37 +6,10 @@ namespace VL
 {
     public class Bootstrap : MonoBehaviour
     {
-        float _timer;
-        bool _isReady;
-
-        void Start()
+        IEnumerator Start()
         {
-            _timer = 0;
-            StartGameIfReady();
-        }
-
-        void Update()
-        {
-            if (_isReady)
-            {
-                return;
-            }
-
-            _timer += Time.unscaledDeltaTime;
-            if (_timer > 0.01)
-            {
-                _timer = 0;
-                StartGameIfReady();
-            }
-        }
-
-        void StartGameIfReady()
-        {
-            if (YandexGamesManager.Instance.IsInitialized())
-            {
-                _isReady = true;
-                SceneManager.LoadScene(Consts.MainMenuSceneName);
-            }
+            yield return new WaitUntil(YandexGamesManager.Instance.IsInitialized);
+            SceneManager.LoadScene(Consts.MainMenuSceneName);
         }
     }
 }
